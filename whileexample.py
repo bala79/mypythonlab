3a2f3ca508f46fea7848c355a65d9fa0df6c6b09
Balachandar <bala.chandar@gmail.com>
2018-09-08 10:25:02 -0700

    While example

---
+++ whileexample.py
@@ -0,0 +1,11 @@
+counter=0
+while counter < 5:
+    print("Hello "+str(counter))
+    counter=counter+1
+
+counter=0
+while True:
+    print("World "+str(counter))
+    counter= counter+1
+    if (counter >=5 ):
+        break
