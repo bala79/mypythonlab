36bf4301908af78e898fa5e9ba82db5eb3cbc7f9
Balachandar <bala.chandar@gmail.com>
2018-09-19 05:35:50 -0700

    added timer

--- sonnetlookup_highperf_set.py
+++ sonnetlookup_highperf_set.py
@@ -1,10 +1,13 @@
+import time
 sowpod_set = set(sowpod.strip() for sowpod in open("sowpods.txt").readlines())
 sonnets = [sonnet.strip() for sonnet in open("sonnet_words.txt").readlines()]
 
 counter = 0
+start = time.time()
 for sonnet in sonnets:
     if(sonnet not in sowpod_set):
         counter = counter +1
         print(sonnet +"Not in sowpod")
-
+end = time.time()
+print("Total time taken %.2f" % (end-start))
 print("Total number of sonnets not in sowpod is %d" % counter)
--- sonnetlookup_lowperf.py
+++ sonnetlookup_lowperf.py
@@ -1,3 +1,4 @@
+import time
 sowpods = open("sowpods.txt").readlines()
 sowpods = [sowpod.strip() for sowpod in sowpods]
 
@@ -5,9 +6,11 @@ sonnets =  open("sonnet_words.txt").readlines()
 sonnets = [sonnet.strip() for sonnet in sonnets]
 
 counter = 0
+start =  time.time()
 for sonnet in sonnets:
     if(sonnet not in sowpods):
         counter = counter +1
         print(sonnet +"Not in sowpod")
-
+end = time.time()
+print("Time Taken %.1f" % (end-start))
 print("Total number of sonnets not in sowpod is %d" % counter)
